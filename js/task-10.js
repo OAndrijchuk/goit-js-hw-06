7e4f74147f0e7c1facc8e2b9bb6cf125a444a28a
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-06-24 22:01:40 +0300

    Olga help me task 10
    12345

--- js/task-10.js
+++ js/task-10.js
@@ -9,11 +9,12 @@ destroyBtnRef.addEventListener("click", destroyBoxes);
 
 function createBoxesGeneral() {
   const countOfElements = Number.parseInt(inputRef.value);
-  if (!isNaN(countOfElements)) {
+  if (countOfElements > 0 && countOfElements < 101 && !isNaN(countOfElements)) {
+    generalContRef.innerHTML = "";
     generalContRef.append(...createBoxes(countOfElements));
     return;
   }
-  alert("Виберіть кількість елементів");
+  alert("Виберіть кількість елементів від 1 до 100");
 }
 
 function destroyBoxes() {
@@ -22,27 +23,15 @@ function destroyBoxes() {
 
 function createBoxes(amount) {
   const elements = [];
-  let width = 30;
-  let height = 30;
 
   for (let i = 0; i < amount; i += 1) {
-    if (!i) {
-      elements.push(createElement(width, height));
-      continue;
-    }
-    width += 10;
-    height += 10;
-    elements.push(createElement(width, height));
+    const elem = document.createElement("div");
+    elem.style.width = `${30 + i * 10}px`;
+    elem.style.height = `${30 + i * 10}px`;
+    elem.style.backgroundColor = getRandomHexColor();
+    elements.push(elem);
   }
   return elements;
-}
-
-function createElement(width, height) {
-  const elem = document.createElement("div");
-  elem.style.width = `${width}px`;
-  elem.style.height = `${height}px`;
-  elem.style.backgroundColor = getRandomHexColor();
-  return elem;
 }
 
 function getRandomHexColor() {
